8d60f42b3a016a8c515f4b684522690df91937a8
Mackie <cameronfarina@gmail.com>
2019-06-21 14:48:51 -0400

    creating music app

---
+++ rails/w5d5/music-app/app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+
+  def new
+    render :new
+  end
+
+  def create
+    user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+    if user
+      login(user)
+      redirect_to user_url(user.id)
+    else
+      flash.now[:errors] = ""
+      render :new
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to new_session_url
+  end
+end
---
+++ rails/w5d5/music-app/app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+
+  def show
+    render :show
+  end
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login!(@user)
+      flash[:success] = "Welcome! Your account has been created successfully!"
+      redirect_to users_url
+    else
+      flash[:errors] = @user.errors.full_messages
+      render :new, status: 422
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password_digest, :session_token, :password)
+  end
+
+end
