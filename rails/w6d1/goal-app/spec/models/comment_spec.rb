b99265a4006b9ed6f0a0a971f970c0c3519af94c
Mackie <cameronfarina@gmail.com>
2019-06-24 16:30:49 -0400

    Added comment_spec.rb

--- rails/w6d1/goal-app/spec/models/comment_spec.rb
+++ rails/w6d1/goal-app/spec/models/comment_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
+  
+    it { should validate_presence_of(:body) }
+    it { should validate_presence_of(:user_id) }
+    it { should validate_presence_of(:goal_id) }
+    it { should belong_to(:goal).class_name(:Goal) }
+    it { should belong_to(:user).class_name(:User) }
 end
--- rails/w6d1/goal-app/spec/models/goal_spec.rb
+++ rails/w6d1/goal-app/spec/models/goal_spec.rb
@@ -5,5 +5,7 @@ RSpec.describe Goal, type: :model do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:description) }
     it { should validate_presence_of(:user_id) }
+    it { should have_many(:comments).class_name(:Comment) }
+    it { should belong_to(:user).class_name(:User) }
 
 end
