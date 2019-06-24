68427b0a9e60d22d968c7bb4bb47cab83bee41dc
Mackie <cameronfarina@gmail.com>
2019-06-24 16:33:26 -0400

    Added auth_token helper

--- rails/w6d1/goal-app/spec/models/comment_spec.rb
+++ rails/w6d1/goal-app/spec/models/comment_spec.rb
@@ -6,5 +6,5 @@ RSpec.describe Comment, type: :model do
     it { should validate_presence_of(:user_id) }
     it { should validate_presence_of(:goal_id) }
     it { should belong_to(:goal).class_name(:Goal) }
-    it { should belong_to(:user).class_name(:User) }
+    it { should belong_to(:user).class_name(:User) }    
 end
