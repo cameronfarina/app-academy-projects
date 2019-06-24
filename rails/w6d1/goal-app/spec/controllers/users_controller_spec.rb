60e6831761bcf2ffe1e3f1bd5e1c3eb41b11a49c
Mackie <cameronfarina@gmail.com>
2019-06-24 16:53:31 -0400

    Added userscontroller_spec

--- rails/w6d1/goal-app/spec/controllers/users_controller_spec.rb
+++ rails/w6d1/goal-app/spec/controllers/users_controller_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
-# RSpec.describe UsersController, type: :controller do
+RSpec.describe UsersController, type: :controller do
+  describe 'GET #new' do 
+    it 'renders a new user' do
+      get :new
+      expect(response).to render_template(:new)
+    end
+  end
 
-# end
+  describe 'POST #create' do
+    context 'with valid params' do
+        it "redirects to goal index page" do
+            expect(repsonse).to redirect_to(goals_url)
+        end
+
+        it 'logs the user in' do 
+            post :create, params: { user: { username: 'himbleflobbits', password: 'password'} }
+            user = User.find_by(username: 'himbleflobbits')
+
+            expect(session[:session_token]).to eq(user.session_token)
+        end
+    end
+
+    context 'with invalid params' do
+        it "validates presence of username and password " do
+            post :create, params: { user: { username: 'himblobbits', password: ''} }
+            expect(response).to render_template(:new)
+            expect(flash[:errors]).to be_present
+        end
+
+        it "validates password is at least 6 characters long " do
+            post :create, params: { user: { username: 'himblobbits', password: 'whats'} }
+            expect(response).to render_template(:new)
+            expect(flash[:errors]).to be_present
+        end
+    end
+  end
+end
